9959aa5fd7dde4eb1d78797a8656b1a2e01e4172
Aubrey Falconer <a@aubreyfalconer.com>
2021-03-22 22:24:33 -0700

    Updated ProjectManagerSettings with openupm

--- _Admin/SymlinkProprietaryAssets.sh
+++
@@ -1,4 +0,0 @@
-# Symlink everything from the _AssetsProprietary folder into our Assets folder
-ln -v -s ../_AssetsProprietary/* ../Assets/
-
-# Add everything from the _AssetsProprietary folder to our root .gitignore as well
